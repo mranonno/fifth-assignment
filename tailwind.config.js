4eb80085ff6eb00c6c4382bcfb53e6c932b58ce5
mranonno <anonnodas97@gmail.com>
2024-02-18 03:00:31 +0600

    best offer section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {
-        colors: {
-          'primary-color': '#1dd100',
-        },
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        black100: "#030712",
+        black80: "#030712CC",
+        black70: "#030712B3",
+        black60: "#03071299",
+        black50: "#03071280",
+        black40: "#03071266",
+        customGreen: "#1DD100",
       },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
