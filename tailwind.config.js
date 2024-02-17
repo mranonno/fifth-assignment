57b5920957e168f3cff1802412dda5ac4ffbba63
mranonno <anonnodas97@gmail.com>
2024-02-17 23:38:29 +0600

    completed header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          'primary-color': '#1dd100',
+        },
+      },
     },
     plugins: [],
 }
